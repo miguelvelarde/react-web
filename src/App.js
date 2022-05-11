2521c3eb1c2011644b24f8076f356d355ea3e6af
miguelvelarde <miguel.angel.velarde@gmail.com>
2022-05-11 12:11:02 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import configuracion from "./configuracion.json"
 
 function App() {
+
+  const [query, setQuery] = useState('test');
+
+  function doGet(){
+    const api = configuracion.SERVER_URL;
+
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  axios
+    .get(api, { headers })
+    .then((response) => {
+      setQuery(response);
+    })
+    .catch((error) => {
+      setQuery(error);
+    });
+
+    alert(JSON.stringify(query))
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +41,11 @@ function App() {
         >
           Learn React
         </a>
+        <button className='btn alert' onClick={doGet}>Test Get</button>
       </header>
     </div>
   );
 }
+
 
 export default App;
